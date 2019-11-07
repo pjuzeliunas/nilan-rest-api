1b9bbd441f3a5219be5a4e44cb89e18935eb28a0
Povilas Juzeliunas <pjuzeliunas@gmail.com>
2019-11-08 00:10:30 +0100

    Exposing readings

--- app/app.go
+++ app/app.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
 	"../controller"
 )
 
-func main() {
-	var settings = controller.FetchSettings()
-	settings.FanSpeed = controller.FanSpeedLow
-	controller.SendSettings(settings)
+func readings(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(controller.FetchReadings())
 }
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/readings", readings)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// func main() {
+// 	var settings = controller.FetchSettings()
+// 	settings.FanSpeed = controller.FanSpeedLow
+// 	controller.SendSettings(settings)
+// }
